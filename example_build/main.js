54811e71bfbc1307a9dfdce509aded183ac87799
MadSnailDisease <madsnaildisease123@gmail.com>
2020-11-11 14:27:36 -0500

    Initial deployment commit

---
+++ example_build/main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require('electron') 
+const url = require('url') 
+const path = require('path')  
+
+let win  
+
+console.log("Hello");
+
+function createWindow() { 
+   win = new BrowserWindow({width: 800, height: 600}) 
+   win.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'index.html'), 
+      protocol: 'file:', 
+      slashes: true 
+   })) 
+}  
+
+app.on('ready', createWindow) 
