52cc74403b5d53d6f8535ff04a6af229b0ddec36
MadSnailDisease <madsnaildisease123@gmail.com>
2020-11-13 12:27:29 -0500

    Makes builds and deployment work

--- example_build/main.js
+++ example_build/main.js
@@ -1,18 +1,33 @@
-const {app, BrowserWindow} = require('electron') 
-const url = require('url') 
-const path = require('path')  
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
 
-let win  
+let win;
 
-console.log("Hello");
+// function createWindow() { 
+//    win = new BrowserWindow({width: 800, height: 600}) 
+//    win.loadURL(url.format ({ 
+//       pathname: path.join(__dirname, 'index.html'), 
+//       protocol: 'file:', 
+//       slashes: true 
+//    })) 
+// }
 
-function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600}) 
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
-      protocol: 'file:', 
-      slashes: true 
-   })) 
-}  
+const createWindow = () => {
 
-app.on('ready', createWindow) 
+   const mainWindow = new BrowserWindow({
+     width: 800,
+     height: 600,
+     webPreferences: {
+       nodeIntegration: true
+     }
+   });
+ 
+   // and load the index.html of the app.
+   mainWindow.loadFile(path.join(__dirname, '../Public/index.html'));
+ 
+   // Open the DevTools.
+   // mainWindow.webContents.openDevTools();
+ };
+
+app.on('ready', createWindow);
